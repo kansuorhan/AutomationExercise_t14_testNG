e72b0d30498997ba8a1f498ea7f61cc2e4a2bd06
Alparslan <maslan11alp@gmail.com>
2022-10-07 19:12:46 +0300

    20221007

---
+++ src/test/java/tests/TestCase101_alparslan.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class TestCase101_alparslan {
+
+    @Test
+    public void testDeneme() {
+        Driver.getDriver().get(ConfigReader.getProperty("automExerUrl"));
+    }
+}
